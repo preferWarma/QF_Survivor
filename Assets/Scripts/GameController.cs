fe1cf34277b6bfd94f2d4b1b92a01b9cdf225e75
LifeLine <2961884371@qq.com>
2024-02-17 19:35:21 +0800

    新增全局游戏控制器GameController来统一保存注册的数据

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using Lyf.SaveSystem;
+using Lyf.Utils.Singleton;
+using QFramework;
+using UnityEngine;
+
+/// <summary>
+/// 全局游戏逻辑管理
+/// </summary>
+public class GameController : GlobalSingleton<GameController>
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]   // 在场景加载后自动初始化
+    public static void AutoInit()
+    {
+        _ = Instance;   // 初始化自身
+        ResKit.Init();
+        _ = Global.Interface;
+        
+        // 加载永久数据
+        SaveManager.Instance.LoadAllRegister(SaveType.PlayerPrefs);
+    }
+    private void OnApplicationQuit() // 退出游戏时保存永久数据
+    {
+        Debug.LogWarning("游戏结束");
+        SaveManager.Instance.SaveAllRegister(SaveType.PlayerPrefs);
+    }
+}
